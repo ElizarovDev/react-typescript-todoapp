671f5a3ddabe509f9df5755e3b7ca826bb9158cf
Elizarov Vladimir <elizarov@moedelo.org>
2022-04-17 22:33:30 +0300

    create project, initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ToDoItem } from "./Models/ToDoItem";
 
 function App() {
+
+  const items: ToDoItem[] = [
+    { title: 'first', isDone: false },
+    { title: 'second', isDone: true },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello from To Do</h1>
+      <ul>
+        {items.map(item => (<li>{item.title} <input type="checkbox" checked={item.isDone} /></li>))}
+      </ul>
     </div>
   );
 }
